4453bb3f78bf47c8713f80a6d0312a8f0d1e3471
ScottStetkiewicz <themuppetman66@yahoo.com>
2020-09-25 13:17:28 -0400

    Revert "meth3"
    This reverts commit db9b362763338aa2a0cf74be478658d86aeb861d.

--- src/methodology.js
+++ src/methodology.js
@@ -11,10 +11,10 @@ function initFillScopeLists () {
     PARTICIPATING_COUNTIES.forEach(function (countyinfo) {
         let $targetlist;
         switch (countyinfo.profile) {
-            case 'fullmodelCID':
+            case 'fullmodel':
                 $targetlist = $listing_full;
                 break;
-            case 'fullmodelGIN':
+            case 'lite':
                 $targetlist = $listing_lite;
                 break;
             default:
