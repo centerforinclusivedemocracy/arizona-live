db9b362763338aa2a0cf74be478658d86aeb861d
ScottStetkiewicz <themuppetman66@yahoo.com>
2020-09-25 13:15:24 -0400

    meth3

--- src/methodology.js
+++ src/methodology.js
@@ -11,10 +11,10 @@ function initFillScopeLists () {
     PARTICIPATING_COUNTIES.forEach(function (countyinfo) {
         let $targetlist;
         switch (countyinfo.profile) {
-            case 'fullmodel':
+            case 'fullmodelCID':
                 $targetlist = $listing_full;
                 break;
-            case 'lite':
+            case 'fullmodelGIN':
                 $targetlist = $listing_lite;
                 break;
             default:
